00da25da2f627e5863a4841a02fc9fe3d04de6a8
Aaron Brown <aaron@9minutesnooze.com>
2012-11-29 07:15:29 -0500

    initial bind class

--- manifests/bind.pp
+++ manifests/bind.pp
@@ -1,22 +1,23 @@
 class borg::bind {
-  class { 'bind::server' : chroot => false }
+  include ::bind
+  include ::bind::logging
 
-  bind::server::conf { '/etc/named.conf':
-    listen_on_addr    => [ 'any' ],
-    listen_on_v6_addr => [ 'any' ],
-    forwarders        => [ '8.8.8.8', '8.8.4.4' ],
-    allow_query       => [ 'localnets' ],
-    zones             => {
-        'borg.lan' => [
-            'type master',
-            'file "borg.lan"',
-        ],
-        '1.168.192.in-addr.arpa' => [
-            'type master',
-            'file "1.168.192.in-addr.arpa"',
-        ],
-    },
+  class { '::bind::options':
+    listen_on => "any",
   }
 
+  ::bind::logging::channel { "default_debug":
+    channel_type => "file",
+    file         => "data/named.run",
+    severity     => "dynamic",
+  }
 
+  ::bind::zone { 'borg.lan':
+    allow_update => 'none',
+    allow_query  => 'any',
+  }
+
+  ::bind::zone::record {
+    'router,A,borg.lan' : data => '192.168.1.1';
+  } 
 }
